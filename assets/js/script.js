ba60f944ce44873a5d1f74b8cacdefad88aa4873
Jake Provard <provardjake@gmail.com>
2023-07-28 20:29:30 -0600

    added js to get the longitude and lattidude of user entered city

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var searchBtn = document.querySelector("#search-btn");
 var cityList = document.querySelector("#city-list");
 
 
+
 searchBtn.addEventListener("click", function(event){
     event.preventDefault();
 
@@ -25,7 +26,10 @@ searchBtn.addEventListener("click", function(event){
 
 function getCityName(userInput){
     var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+userInput+"&limit=5&appid="+APIKey;
-    
+    var cityLongitude;
+    var cityLatitude;
+    var cityArray = [];
+
     fetch(requestUrl)
     .then(function(response){
         return response.json();
@@ -33,14 +37,38 @@ function getCityName(userInput){
     .then(function(data){
         for(var i = 0; i < data.length; i++){
             var cityListItem = document.createElement("button");
-            cityListItem.setAttribute("type", "button")
+            cityListItem.setAttribute("type", "button");
+            cityListItem.setAttribute("data-array-index", i);
             cityListItem.setAttribute("class", "list-group-item list-group-item-action list-group-item-secondary");
-            cityListItem.textContent = data[i].name+", "+data[i].state;
+            if(data[i].state === undefined || data[i].country !== "US"){
+                cityListItem.textContent = data[i].name+", "+data[i].country;
+            }
+            else{
+                cityListItem.textContent = data[i].name+", "+data[i].state;
+            }
+            cityArray.push(data[i]);
             cityList.appendChild(cityListItem);
         }
         cityList.style.display = "flex";
+
+        document.addEventListener("click", function(event){
+            event.preventDefault();
+            const target = event.target.closest(".list-group-item");
+
+            if(target){
+                var cityArrayIndex = parseInt(target.getAttribute("data-array-index"));
+                cityLatitude = cityArray[cityArrayIndex].lat;
+                cityLongitude = cityArray[cityArrayIndex].lon;
+
+                getWeather(cityLatitude, cityLongitude);
+            }
+            
+            
+            
+        })
     })
 }
+
 
 function getZipCode(userInput){
     var requestUrl = "http://api.openweathermap.org/geo/1.0/zip?zip="+userInput+",US&appid="+APIKey;
@@ -51,13 +79,18 @@ function getZipCode(userInput){
     })
     .then(function(data){
         console.log(data);
-        for(var i = 0; i < data.length; i++){
-            var cityListItem = document.createElement("button");
-            cityListItem.setAttribute("type", "button")
-            cityListItem.setAttribute("class", "list-group-item list-group-item-action list-group-item-secondary");
-            cityListItem.textContent = data[i].name+", "+data[i].country;
-            cityList.appendChild(cityListItem);
-        }
-        cityList.style.display = "flex";
     })
+}
+
+function getWeather(lat, lon){
+    var requestURl = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey+"&units=imperial";
+
+    fetch(requestURl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+
 }
